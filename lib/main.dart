b8d977b73ab3e8dfe555d5828f35c525ab52c91b
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-09-12 00:30:58 +0300

    update show ride data

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    // options: DefaultFirebaseOptions.currentPlatform,
   );
 
   FirebaseNotifications.init();
