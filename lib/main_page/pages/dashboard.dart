fa0c868fa3604af4eb8fa495f97ae67437ede8cd
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-09-13 22:03:03 +0300

    commit

--- lib/main_page/pages/dashboard.dart
+++ lib/main_page/pages/dashboard.dart
@@ -34,9 +34,9 @@ class _DashBoardState extends State<DashBoard> {
   Widget fragment(int index) {
     switch (index) {
       case 0:
-        return const Home();
+        return const SizedBox();
       case 1:
-        return const Statistics();
+        return const SizedBox();
       case 2:
         return const Profile();
       case 3:
